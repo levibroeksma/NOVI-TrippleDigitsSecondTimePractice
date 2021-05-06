af3d145f069af41dcc2360aca97cd07e420304f3
School <School@Levis-oude-bakbeest.local>
2021-05-06 15:22:28 +0200

    finished game

--- src/nl/levi/broeksma/Main.java
+++ src/nl/levi/broeksma/Main.java
@@ -7,53 +7,44 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // generate numbers to guess
-            // strore number in variables
+        int firstDigit;
+        int secondDigit;
+        int thirdDigit;
 
-        // if correct, player wins
-            // if player wins, congratulate, game is finished. Ask if he wants to play again
-        // if incorret, player gets to play another round if total rounds is less then 3
-            // if player guessed incorrectly for 3rd time, player has lost, ask if he want's to play again
-        for (int i = 0; i < 3; i++) {
-            int firstDigit;
-            int secondDigit;
-            int thirdDigit;
+        int maximum = 9;
 
-            int maximum = 9;
+        Random randomizer = new Random();
 
-            Random randomizer = new Random();
+        firstDigit = randomizer.nextInt(maximum) + 1;
+        secondDigit = randomizer.nextInt(maximum) + 1;
+        thirdDigit = randomizer.nextInt(maximum) + 1;
 
-            firstDigit = randomizer.nextInt(maximum) + 1;
-            secondDigit = randomizer.nextInt(maximum) + 1;
-            thirdDigit = randomizer.nextInt(maximum) + 1;
+        // as a hint the player gets a sum and a multiplication of the numbers
+        int sumOfDigits = firstDigit + secondDigit + thirdDigit;
+        int multiplicationOfDigits = firstDigit * secondDigit * thirdDigit;
 
-            // as a hint the player gets a sum and a multiplication of the numbers
-            int sumOfDigits = firstDigit + secondDigit + thirdDigit;
-            int multiplicationOfDigits = firstDigit * secondDigit * thirdDigit;
+        System.out.println("The sum of the Digits is: " + sumOfDigits);
+        System.out.println("The multiplication of the Digits is: " + multiplicationOfDigits);
 
-            System.out.println("The sum of the Digits is: " + sumOfDigits);
-            System.out.println("The multiplication of the Digits is: " + multiplicationOfDigits);
-
-            // Let the player enter 3 numbers between 1 and 9
+        int maxTimesToPlay = 3;
+        for (int i = 0; i < maxTimesToPlay; i++) {
             System.out.println("Please enter a number between 1 and 9:");
             Scanner input = new Scanner(System.in);
             int guessOne = input.nextInt();
+
             System.out.println("Please enter another number between 1 and 9:");
             int guessTwo = input.nextInt();
+
             System.out.println("Please enter another number between 1 and 9:");
             int guessThree = input.nextInt();
 
             System.out.println("Your guesses where: " + guessOne + ", " + guessTwo + ", " + guessThree);
             // check numbers if they match generated numbers
-            boolean hasPlayerWon =
-                    guessOne == firstDigit && guessTwo == secondDigit && guessThree == thirdDigit ||
-                            guessOne == firstDigit && guessTwo == thirdDigit && guessThree == secondDigit ||
-                            guessOne == secondDigit && guessTwo == firstDigit && guessThree == thirdDigit ||
-                            guessOne == secondDigit && guessTwo == thirdDigit && guessThree == firstDigit ||
-                            guessOne == thirdDigit && guessTwo == secondDigit && guessThree == firstDigit ||
-                            guessOne == thirdDigit && guessTwo == firstDigit && guessThree == secondDigit;
 
-            // order of guessed numbers should not matter
+            int sumOfGuesses = guessOne + guessTwo + guessThree;
+            int multiplicationOfGuesses = guessOne * guessTwo * guessThree;
+            boolean hasPlayerWon = sumOfDigits == sumOfGuesses && multiplicationOfDigits == multiplicationOfGuesses;
+
             if(hasPlayerWon == true) {
                 System.out.println("Congratulations! You have beaten the game.");
                 System.out.println("\n");
@@ -62,7 +53,39 @@ public class Main {
                 System.out.println("\n");
             }
 
+            // Ask player if he wants to continue playing
+            boolean playAgain = true;
+
+            System.out.println("Do you want to play another game? Please enter y/n");
+
+            String continuePlaying = input.next();
+
+            switch(continuePlaying) {
+                case "y" :{
+                    System.out.println("You have chosen to play another round");
+                    playAgain = true;
+                    break;
+                }
+                case "n" :{
+                    System.out.println("Thank you for playing, see you next time!");
+                    playAgain = false;
+                    break;
+                }
+                default: {
+                    System.out.println("Wrong value has been entered.");
+                    break;
+                }
+
+            }
+
+            // Terminate game if player choice equals no
+            if(playAgain == false) {
+                break;
+            }
+
         }
+
+        // Notify game has been ended
         System.out.println("Het spel is afgelopen");
 
     }
